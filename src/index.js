e7971ed59f59fe85a9b114043cf96938a0c5db5c
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-07-24 15:42:51 +0300

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const newTaskDescription = document.getElementById("new-task-description").value;
+
+    if (newTaskDescription.trim() !== "") {
+      addTask(newTaskDescription);
+      form.reset();
+    }
+  });
+
+  function addTask(task) {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = task;
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      taskItem.remove();
+    });
+  
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+  }
+  
 });
